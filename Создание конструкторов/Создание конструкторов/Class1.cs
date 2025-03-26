72722a853805f4c837400440434cfdb5fefbe649
cheburek <vovka@mail.com>
2025-03-26 16:35:17 +0300

    sdfhjigndfg

---
+++ Создание конструкторов/Создание конструкторов/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamesInDirectory
+{
+    class GameFolder
+    {
+        private string folderPath;
+        public GameFolder(string folderPath)
+        {
+            this.folderPath = folderPath;
+        }
+
+        // Метод для подсчета количества игр в папке
+        public int CountGames()
+        {
+            if (!Directory.Exists(folderPath))
+            {
+                Console.WriteLine("Папка не найдена!");
+                return 0;
+            }
+
+            // Получаем список всех файлов с расширением .exe в указанной папке
+            string[] gameFiles = Directory.GetFiles(folderPath, "*.exe", SearchOption.TopDirectoryOnly);
+
+            // Возвращаем количество игр
+            return gameFiles.Length;
+        }
+
+        // Метод для отображения количества игр
+        public void ShowGameCount()
+        {
+            int gameCount = CountGames();
+            Console.WriteLine($"Количество игр в папке: {gameCount}");
+        }
+    }
---
+++ Создание конструкторов/Создание конструкторов/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        string gamesFolderPath = Path.Combine(desktopPath, "Игры");
+        GameFolder gameFolder = new GameFolder(gamesFolderPath);
+        gameFolder.ShowGameCount();
+    }
+}
+}
